b7354f9391c7527297239992bab57f59bba2482b
August <fshrmn.august@gmail.com>
2026-05-31 12:22:33 +0300

    templates

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,14 +1,29 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("index"))
+
+	tmpl, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
+	if err = tmpl.Execute(w, ""); err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
 }
 
 func TasksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("tasks"))
-
 }
 
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
