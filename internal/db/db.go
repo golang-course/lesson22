f68fedc17557c44749d41ac54a48fec5c0128540
August <fshrmn.august@gmail.com>
2026-05-31 12:52:23 +0300

    select all clients

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,51 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+/*
+SelectAllClients() []Client
+SelectAllTasks() []Tasks
+*/
+
+func SelectAllClients() []Client {
+	db, err := sql.Open("sqlite", "lesson22.db")
+	if err != nil {
+		log.Println(err)
+		return []Client{}
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("ping db:", err)
+	}
+
+	rows, err := db.Query("SELECT * FROM clients")
+	if err != nil {
+		log.Println(err)
+		return []Client{}
+	}
+
+	defer rows.Close()
+	clients := []Client{}
+
+	for rows.Next() {
+		ph := Client{}
+		err := rows.Scan(&ph.Id, &ph.Name, &ph.Created, &ph.WasOnline)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		clients = append(clients, ph)
+	}
+	return clients
+}
+
+func SelectAllTasks() []Task {
+
+}
