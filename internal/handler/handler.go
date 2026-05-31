90f0fa119d442bac1a376e493e3ae1a90c1155fa
August <fshrmn.august@gmail.com>
2026-05-31 12:31:13 +0300

    placeholders in db

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -23,11 +23,31 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func TasksHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("tasks"))
+	tmpl, err := template.ParseFiles("templates/tasks.html")
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
+	if err = tmpl.Execute(w, ""); err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
 }
 
 func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("create-task"))
+	tmpl, err := template.ParseFiles("templates/create-task.html")
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
+	if err = tmpl.Execute(w, ""); err != nil {
+		log.Println(err)
+		fmt.Fprintln(w, err)
+		return
+	}
 
 }
 
