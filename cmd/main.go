a2c27d494588f68cbfe3b0380c311308d30096a8
August <fshrmn.august@gmail.com>
2026-06-02 19:07:25 +0300

    css in other file

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,14 @@ import (
 
 func main() {
 	r := http.NewServeMux()
+
 	r.HandleFunc("/", handler.IndexHandler)
 	r.HandleFunc("/tasks", handler.TasksHandler)
 	r.HandleFunc("/create-task", handler.CreateTaskHandler)
 	r.HandleFunc("/ask-task", handler.AskTaskHandler)
+
+	fs := http.FileServer(http.Dir("static"))
+	r.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Println("Server starting...")
 	if err := http.ListenAndServe(":8080", r); err != nil {
